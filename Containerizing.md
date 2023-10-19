a60f9e409015ba92691ef12cb2bea2c27b2200e3
amnahsiddiqa <amnahsiddiqa@gmail.com>
2023-10-19 17:23:01 -0400

    Add initial files

---
+++ Containerizing.md
@@ -0,0 +1,9 @@
+---
+title: "Containerizing"
+author: "Amnah Siddiqa"
+date: "2023-10-19"
+---
+
+
+
+### In Progress
---
+++ utils.R
@@ -0,0 +1,16 @@
+# rmarkdown::render("index.Rmd", output_dir = "docs")
+# rmarkdown::render("Acquiring_and_sharing_data.Rmd", output_dir = "docs")
+# rmarkdown::render("Containerizing.Rmd", output_dir = "docs")
+# rmarkdown::render("Publishing.Rmd", output_dir = "docs")
+# rmarkdown::render("Working_with_hpc.Rmd", output_dir = "docs")
+# rmarkdown::render("Publishing.Rmd", output_dir = "docs")
+
+
+rmarkdown::render("index.Rmd")
+rmarkdown::render("Acquiring_and_sharing_data.Rmd")
+rmarkdown::render("Containerizing.Rmd")
+rmarkdown::render("Publishing.Rmd")
+rmarkdown::render("Working_with_hpc.Rmd")
+rmarkdown::render("git_and_github.Rmd")
+rmarkdown::render("Publishing.Rmd")
+rmarkdown::render("Organizing_projects.Rmd")
